e1d427d075a2b344058733e52a263a49fe4ff6bd
nnglebanov <n.n.glebanov@gmail.com>
2019-01-19 20:01:01 +0300

    Задание №2: Создать вспомогательные методы и классы

--- test_add_group.py
+++ test_add_group.py
@@ -2,6 +2,8 @@
 from selenium import webdriver
 import unittest
 
+from groups import Group
+
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -10,29 +12,38 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("groups").click()
+        self.open_addressbook_page(wd)
+        self.login(wd,"admin","secret")
+        self.to_groups(wd)
+        self.add_new_group(wd, Group("name", "header", "footer"))
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def add_new_group(self, wd, group):
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("TestGroup")
-        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("TestHeader")
-        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("TestFooter")
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
+
+    def to_groups(self, wd):
+        wd.find_element_by_xpath("//a[text()='groups']").click()
+
+    def open_addressbook_page(self, wd):
+        wd.get("http://localhost/addressbook")
+
+    def login(self, wd, user, password):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(user)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def tearDown(self):
         self.wd.quit()
