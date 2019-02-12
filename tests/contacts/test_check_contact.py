0e446f5b3b2b4df968685d6c9065fa7902ec229d
nnglebanov <n.n.glebanov@gmail.com>
2019-02-12 14:06:44 +0300

    Задание №14: Реализовать проверки для всех полей контакта на главной странице fix#1

--- tests/contacts/test_check_contact.py
+++ tests/contacts/test_check_contact.py
@@ -9,8 +9,6 @@ def test_check_contact(app):
     ln1, fn1, ad1, em1, ph1 = app.contacts.get_info_from_main_page_by_num(index)
     app.contacts.to_edit_contact_by_num(index)
     ln2, fn2, ad2, em2, ph2 = app.contacts.get_info_from_edit_page()
-    assert ln1 == ln2
-    assert fn2 == fn2
-    assert ad1 == ad2
-    assert em1 == em2
-    assert ph1 == ph2
+    fs = "%s %s %s %s %s" % (ln1, fn1, ad1, em1, ph1)
+    ss = "%s %s %s %s %s" % (ln2, fn2, ad2, em2, ph2)
+    assert fs == ss
