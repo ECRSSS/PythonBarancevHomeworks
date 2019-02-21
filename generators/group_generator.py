852c10f97d53cc599e5624891cd9ab69eef088e3
nnglebanov <n.n.glebanov@gmail.com>
2019-02-21 12:54:46 +0300

    Задание №18: Реализовать генераторы тестовых данных

---
+++ generators/group_generator.py
@@ -0,0 +1,13 @@
+from model.groups import Group
+from utils.utils import random_string
+import os, jsonpickle
+
+
+def generate_groups_data(num_of_elms):
+    testdata = [
+        Group(random_string("name", 20), random_string("header", 20), random_string("footer", 20), None)
+        for i in range(num_of_elms)
+    ]
+    file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/groups.json")
+    with open(file, "w") as f:
+        f.write(jsonpickle.encode(testdata))
