4a428c5955280f8da36364645f9fcd18655c015d
nnglebanov <n.n.glebanov@gmail.com>
2019-03-01 13:02:17 +0300

    Задание 14 и 21

--- tests/contacts/test_check_contact.py
+++ tests/contacts/test_check_contact.py
@@ -6,7 +6,9 @@ def test_check_contact(app):
     app.navigation.to_contacts()
     old_contacts = app.contacts.get_info_from_main_page_list()
     for n in range(len(old_contacts)):
-        ln1, fn1, ad1, em1, ph1 = app.contacts.get_info_from_main_page_by_num(n)
+        app.contacts.to_edit_contact_by_num(n)
+        ln1, fn1, ad1, em1, ph1 = app.contacts.get_info_from_edit_page()
+        app.navigation.to_contacts()
         ln2, fn2, ad2, em2, ph2 = old_contacts[n]
         fs = "%s %s %s %s %s" % (ln1, fn1, ad1, em1, ph1)
         ss = "%s %s %s %s %s" % (ln2, fn2, ad2, em2, ph2)
