df190c7331f03f9d0e3c66a2ce72176d728f8cb0
nnglebanov <n.n.glebanov@gmail.com>
2019-03-10 06:28:55 +0300

    21 и 14 задание

--- tests/contacts/test_check_contact.py
+++ tests/contacts/test_check_contact.py
@@ -6,6 +6,6 @@ def test_check_contact(app):
     app.navigation.to_contacts()
     orm_contacts = app.orm.get_contacts_as_strings_list()
     main_page_contacts = app.contacts.get_info_from_main_page_as_strings_list()
-    for n in range(len(orm_contacts)):
-        print("ORM: " + orm_contacts[n])
-        print("Main: " + main_page_contacts[n])
+    orm_contacts.sort()
+    main_page_contacts.sort()
+    assert orm_contacts == main_page_contacts
